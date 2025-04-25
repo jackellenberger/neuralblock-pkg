82a33cd2a7667286513efb80e79433ef25060e12
jellenberger <jellenberger@google.com>
2025-04-25 18:39:31 +0000

    add package_data

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     package_dir={'': '.'}, # Look for packages in the current directory
     packages=find_packages(where='.'), # Find packages in the current directory
     include_package_data=True, # Include non-code files specified in MANIFEST.in
+    # Explicitly include package data for the 'neuralblock' package
+    package_data={
+        'neuralblock': ['models/*'], # Include all files in the models subdirectory
+    },
     install_requires=[
         "tensorflow-cpu",
         "numpy"
